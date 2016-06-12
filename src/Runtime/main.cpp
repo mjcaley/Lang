a58f93a62cbbbfe7331f93013d56335879603f17
Michael Caley <mjcaley@darkarctic.com>
2016-06-12 14:39:31 -0400

    Corrected headers in runtime's main

--- src/Runtime/main.cpp
+++ src/Runtime/main.cpp
@@ -1,4 +1,4 @@
-#include "StackMachineFile.hpp"
+#include "LangFile.hpp"
 #include "VM/VM.hpp"
 #include <boost/program_options.hpp>
 
@@ -7,6 +7,7 @@ int main(int argc, char* argv[])
 {
     using std::cout;
     using std::endl;
+    using std::string;
     namespace po = boost::program_options;
     using namespace Lang;
     
@@ -16,21 +17,30 @@ int main(int argc, char* argv[])
     ("source", po::value<std::string>(), "source file")
     ;
     
+    po::positional_options_description p;
+    p.add("source", 1);
+    
     po::variables_map var_map;
-    po::store(po::parse_command_line(argc, argv, desc), var_map);
+    po::store(po::command_line_parser(argc, argv).
+              options(desc).positional(p).run(), var_map);
     po::notify(var_map);
     
     if (var_map.count("help"))
     {
         cout << desc << endl;
     }
-    else if (auto src = var_map.count("source"))
+    else if (var_map.count("source"))
     {
-        auto file = LangFile::create(src);
+        auto file = LangFile::create(var_map["source"].as<string>());
         
-        auto vm = VM();
-        vm.loadProgram(file);
+        auto vm = VM::VM();
+        vm.debug = true;
+        vm.loadProgram(std::move(file));
         vm.run();
+    }
+    else
+    {
+        cout << desc << endl;
     }
     
     return 0;
