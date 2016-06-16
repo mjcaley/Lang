2b1dda7ac1107e1e0fdc87759cd5427564ee1934
Michael Caley <mjcaley@darkarctic.com>
2016-06-16 11:42:55 -0400

    Added tests for Bytecode parsing

---
+++ src/Test/Bytecode.cpp
@@ -0,0 +1,201 @@
+#include <catch.hpp>
+
+#include "Bytecode/InstructionSet.hpp"
+#include "Bytecode/AST.hpp"
+#include "Bytecode/Grammar.hpp"
+#include <string>
+#include <boost/spirit/home/x3.hpp>
+
+
+TEST_CASE( "Nullary parsing", "[Bytecode]")
+{
+    namespace x3 = boost::spirit::x3;
+    namespace AST = Lang::Bytecode::AST;
+    namespace Grammar = Lang::Bytecode::Grammar;
+    
+    std::string program;
+    std::string::const_iterator iter, end;
+    bool result { false };
+    AST::Nullary ast;
+    auto& grammar = Grammar::nullary_op;
+    
+    // halt
+    program = "halt";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // pop
+    program = "pop";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // add
+    program = "add";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // sub
+    program = "sub";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // mul
+    program = "mul";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // eq
+    program = "eq";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // lt
+    program = "lt";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // gt
+    program = "gt";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // prnt
+    program = "prnt";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // ret
+    program = "ret";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // dup
+    program = "dup";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+}
+
+TEST_CASE( "Unary parsing", "[Bytecode]")
+{
+    namespace x3 = boost::spirit::x3;
+    namespace AST = Lang::Bytecode::AST;
+    namespace Grammar = Lang::Bytecode::Grammar;
+    
+    std::string program;
+    std::string::const_iterator iter, end;
+    bool result { false };
+    AST::Unary ast;
+    auto& grammar = Grammar::unary_op;
+    
+    // push
+    program = "push 1";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // load
+    program = "load 2";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // store
+    program = "store 3";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+}
+
+TEST_CASE( "Label parsing", "[Bytecode]")
+{
+    namespace x3 = boost::spirit::x3;
+    namespace AST = Lang::Bytecode::AST;
+    namespace Grammar = Lang::Bytecode::Grammar;
+    
+    std::string program = "label:";
+    auto iter = program.begin();
+    auto end = program.end();
+    AST::Label ast;
+    auto& grammar = Grammar::label;
+    
+    bool result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    
+    REQUIRE((result && iter == end) == true);
+}
+
+TEST_CASE( "Jump parsing", "[Bytecode]")
+{
+    namespace x3 = boost::spirit::x3;
+    namespace AST = Lang::Bytecode::AST;
+    namespace Grammar = Lang::Bytecode::Grammar;
+    
+    std::string program;
+    std::string::const_iterator iter, end;
+    bool result { false };
+    AST::Jump ast;
+    auto& grammar = Grammar::jump_op;
+    
+    // jmp
+    program = "jmp 1";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // jt
+    program = "jt 2";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+    
+    // jf
+    program = "jf 3";
+    iter = program.begin();
+    end = program.end();
+    result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    REQUIRE((result && iter == end) == true);
+}
+
+TEST_CASE( "Call parsing", "[Bytecode]")
+{
+    namespace x3 = boost::spirit::x3;
+    namespace AST = Lang::Bytecode::AST;
+    namespace Grammar = Lang::Bytecode::Grammar;
+    
+    std::string program = "call 0 3";
+    auto iter = program.begin();
+    auto end = program.end();
+    AST::Call ast;
+    auto& grammar = Grammar::call_op;
+    
+    bool result = x3::phrase_parse(iter, end, grammar, Grammar::skipper, ast);
+    
+    REQUIRE((result && iter == end) == true);
+}
